52dadbcf9fac6ec42ab3134e82ea55a6d26428bd
cts33 <chentaishan-19@163.com>
2023-03-20 20:55:59 +0800

    完善  fileObserver

--- myapplication2/src/main/java/com/example/myapplication2/DirectoryObserver.kt
+++ myapplication2/src/main/java/com/example/myapplication2/DirectoryObserver.kt
@@ -9,21 +9,28 @@ private const val TAG = "DirectoryObserver"
 public var fileObservers: MutableMap<String, DirectoryObserver.SubDirectory>? = null
 
 class DirectoryObserver(private val directionPath: String) {
-
-    init {
+    private var _iEventChangedListener: IEventChangedListener? = null
+    private fun loadRootPath() {
         fileObservers = HashMap<String, SubDirectory>()
         if (!directionPath.isNullOrEmpty()) {
             val rootFile = File(directionPath)
+            val subDirectory = SubDirectory(
+                rootFile.absolutePath,
+                FileObserver.ALL_EVENTS,
+                _iEventChangedListener!!
+            )
+            fileObservers?.set(rootFile.absolutePath, subDirectory)
             if (rootFile.exists()) {
                 listDirectories(rootFile)
             }
         }
     }
 
-    fun start() {
-        val rootFile = File(directionPath)
-        val subDirectory = SubDirectory(rootFile.absolutePath, FileObserver.ALL_EVENTS)
-        fileObservers?.set(rootFile.absolutePath, subDirectory)
+    fun start(iEventChangedListener: IEventChangedListener) {
+        _iEventChangedListener = iEventChangedListener
+
+        loadRootPath()
+
         for (item in fileObservers!!.values) {
             Log.d(TAG, "start() called item=${item.file}")
             item.startWatching()
@@ -34,6 +41,7 @@ class DirectoryObserver(private val directionPath: String) {
         for (item in fileObservers!!.values) {
             item.stopWatching()
         }
+        fileObservers!!.clear()
     }
 
     private fun listDirectories(direction: File) {
@@ -41,23 +49,39 @@ class DirectoryObserver(private val directionPath: String) {
         for (item in listFiles) {
             if (item.isDirectory) {
                 Log.d(TAG, "listDirectories: item=$item")
-                val subDirectory = SubDirectory(item.absolutePath, FileObserver.ALL_EVENTS)
+                val subDirectory =
+                    SubDirectory(
+                        item.absolutePath,
+                        FileObserver.ALL_EVENTS,
+                        _iEventChangedListener!!
+                    )
                 fileObservers!![item.absolutePath] = subDirectory
                 listDirectories(item)
             }
         }
     }
 
-    class SubDirectory(val file: String, mask: Int) : FileObserver(file, mask) {
+
+    enum class FileEvent {
+        CREATE_FILE,
+        CREATE_DIRECTORY,
+        DELETE_FILE,
+        DELETE_DIRECTORY,
+        MODIFY_FILE
+    }
+
+
+    class SubDirectory(
+        val file: String,
+        mask: Int,
+        private val iEventChangedListener: IEventChangedListener?
+    ) : FileObserver(file, mask) {
         override fun onEvent(event: Int, path: String?) {
-            Log.d(TAG, "onEvent()   event = $event  path = $file/$path -----")
             addDirectory(file + File.separator + path)
             when (event) {
-//                (ACCESS or MODIFY or ATTRIB or CLOSE_WRITE
-//                        or CLOSE_NOWRITE or OPEN or MOVED_FROM or MOVED_TO or DELETE or CREATE
-//                        or DELETE_SELF or MOVE_SELF)
                 MODIFY -> {
-                    Log.d(TAG, "onEvent()   MODIFY file = $file  path = $path,")
+                    Log.d(TAG, "onEvent()   MODIFY file = ${file+File.separator+path}  path = $path,")
+                    iEventChangedListener?.onChange(file+File.separator+path, FileEvent.MODIFY_FILE)
                 }
                 ACCESS -> {
                     Log.d(TAG, "onEvent()   ACCESS file = $file  path = $path,")
@@ -82,30 +106,43 @@ class DirectoryObserver(private val directionPath: String) {
                     Log.d(TAG, "onEvent()   MOVED_TO file = $file  path = $path,")
                 }
                 CREATE -> {
-                    Log.d(TAG, "onEvent()   CREATE file = $file  path = $path,")
+                    Log.d(TAG, "onEvent()   CREATE file = ${file+File.separator+path}  path = $path,")
+                    iEventChangedListener?.onChange(file+File.separator+path, FileEvent.CREATE_FILE)
                 }
                 DELETE -> {
-                    Log.d(TAG, "onEvent()   DELETE file = $file  path = $path,")
+                    Log.d(TAG, "onEvent()   DELETE file = ${file+File.separator+path}  path = $path,")
+                    iEventChangedListener?.onChange(file+File.separator+path, FileEvent.DELETE_FILE)
                 }
                 MOVE_SELF -> {
                     Log.d(TAG, "onEvent()   MOVE_SELF file = $file  path = $path,")
+
                 }
                 DELETE_SELF -> {
                     Log.d(TAG, "onEvent()   DELETE_SELF file = $file  path = $path,")
                     if (fileObservers!!.contains(file)) {
+                        Log.d(TAG, "onEvent: remove-file")
                         fileObservers!!.remove(file)
+                        iEventChangedListener?.onChange(file, FileEvent.DELETE_DIRECTORY)
                     }
                 }
             }
         }
 
         private fun addDirectory(path: String) {
+            Log.d(TAG, "addDirectory() called with: path = $path")
             val file = File(path)
-            if (file.exists() && file.isDirectory) {
-                fileObservers!![path] = SubDirectory(path, ALL_EVENTS)
+            if (file.exists() && file.isDirectory && !fileObservers!!.containsKey(path)) {
+                Log.d(TAG, "addDirectory: ${file.absolutePath}")
+                fileObservers!![path] =
+                    SubDirectory(path, FileObserver.ALL_EVENTS, iEventChangedListener)
                 fileObservers!![path]!!.startWatching()
+                iEventChangedListener?.onChange(file.absolutePath, FileEvent.CREATE_DIRECTORY)
             }
         }
+    }
+
+    interface IEventChangedListener {
+        fun onChange(path: String, event: FileEvent)
     }
 
 }
--- myapplication2/src/main/java/com/example/myapplication2/MainActivity.kt
+++ myapplication2/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.myapplication2
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+import kotlin.random.Random
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    var directoryObserver:DirectoryObserver?=null
-
+    var directoryObserver: DirectoryObserver? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         directoryObserver = DirectoryObserver(cacheDir.absolutePath)
-        directoryObserver!!.start()
+        directoryObserver!!.start(object : DirectoryObserver.IEventChangedListener {
+            override fun onChange(path: String, event: DirectoryObserver.FileEvent) {
+                Log.d(TAG, "onChange() called with: path = $path, event = $event")
+            }
+        })
+        findViewById<Button>(R.id.btn_send).setOnClickListener {
+            val file = File(cacheDir.absolutePath+ File.separator+"1/1.md")
+            if (file.exists()){
+                val text = Random(4343).toString()
+                file.appendText(text,Charsets.UTF_8)
+            }
+        }
     }
 
     override fun onDestroy() {
